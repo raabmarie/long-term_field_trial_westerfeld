d3458c7ac4d94bb7ed69f9bf1404e7bbe3282a46
laschuet <laschuet@users.noreply.github.com>
2024-10-01 17:57:22 +0200

    Add another preparation example

---
+++ examples/preparation/sowing.py
@@ -0,0 +1,32 @@
+import pandas as pd
+from common import prepare_table_experiment
+
+# Load CSV files
+df_sowing = pd.read_csv("../../lte_westerfeld.V1_0_SOWING.csv")
+df_seed_stock = pd.read_csv("../../lte_westerfeld.V1_0_SEED_STOCK.csv")
+df_plant_variety = pd.read_csv("../../lte_westerfeld.V1_0_PLANT_VARIETY.csv")
+
+# Add SEED STOCK information
+df_sowing = pd.merge(
+    df_sowing,
+    df_seed_stock[["Seed_Stock_ID", "Plant_Variety_ID"]],
+    on=["Seed_Stock_ID"],
+    how="left",
+)
+
+# Add PLANT VARIETY information
+df_sowing = pd.merge(
+    df_sowing,
+    df_plant_variety[["Plant_Variety_ID", "Name"]],
+    on=["Plant_Variety_ID"],
+    how="left",
+)
+df_sowing = df_sowing.rename(columns={"Name": "Plant_Variety"})
+
+# Drop merged identifier columns
+df_sowing = df_sowing.drop(columns=["Plant_Variety_ID", "Seed_Stock_ID"])
+
+# Add experiment information
+df_sowing = prepare_table_experiment(df_sowing)
+
+print(list(df_sowing.columns))
