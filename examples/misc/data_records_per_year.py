df77914fa7581c04695a2cd82a8e0107ba02ed64
laschuet <laschuet@users.noreply.github.com>
2024-12-03 10:24:16 +0100

    Revert "Update data_records_per_year.py"
    This reverts commit dc577d02ea3f446af19aea1a05dc6919cf75aec7.

--- examples/misc/data_records_per_year.py
+++ examples/misc/data_records_per_year.py
@@ -16,62 +16,39 @@ df_gene_expression = pd.read_csv("../../lte_westerfeld.V1_0_GENE_EXPRESSION.csv"
 dtypes_fungi = {"Seq_ID": "str", "ACC_Num": "str"}
 df_fungi = pd.read_csv("../../lte_westerfeld.V1_0_FUNGI.csv", dtype=dtypes_fungi)
 dtypes_bacteria = {"Seq_ID": "str", "ACC_Num": "str", "OTU_ID": "str"}
-df_bacteria = pd.read_csv("../../lte_westerfeld.V1_0_BACTERIA.csv", dtype=dtypes_bacteria)
+df_bacteria = pd.read_csv(
+    "../../lte_westerfeld.V1_0_BACTERIA.csv", dtype=dtypes_bacteria
+)
 
 # Count data records per year
 year_column = "Experimental_Year"
 counts_df_crop_rotation = (
     df_crop_rotation.groupby(year_column).size().reset_index(name="counts")
 )
-counts_df_crop_rotation['Category'] = "Experimental_data"
-
 counts_df_experimental_setup = (
     df_experimental_setup.groupby(year_column).size().reset_index(name="counts")
 )
-counts_df_experimental_setup['Category'] = "Experimental_data"
-
 counts_df_plant_protection = (
     df_plant_protection.groupby(year_column).size().reset_index(name="counts")
 )
-counts_df_plant_protection['Category'] = "Field_data" 
-
 counts_df_sowing = df_sowing.groupby(year_column).size().reset_index(name="counts")
-counts_df_sowing['Category'] = "Field_data" 
-
 counts_df_tillage = df_tillage.groupby(year_column).size().reset_index(name="counts")
-counts_df_tillage['Category'] = "Field_data" 
-
 counts_df_fertilization = (
     df_fertilization.groupby(year_column).size().reset_index(name="counts")
 )
-counts_df_fertilization['Category'] = "Field_data" 
-
 counts_df_harvest = df_harvest.groupby(year_column).size().reset_index(name="counts")
-counts_df_harvest['Category'] = "Field_data" 
-
 counts_df_soil_sampling = (
     df_soil_sampling.groupby(year_column).size().reset_index(name="counts")
 )
-counts_df_soil_sampling['Category'] = "Soil_data" 
-
 counts_df_plant_sampling = (
     df_plant_sampling.groupby(year_column).size().reset_index(name="counts")
 )
-counts_df_plant_sampling['Category'] = "Plant_data" 
-
 counts_df_root = df_root.groupby(year_column).size().reset_index(name="counts")
-counts_df_root['Category'] = "Plant_data" 
-
 counts_df_gene_expression = (
     df_gene_expression.groupby(year_column).size().reset_index(name="counts")
 )
-counts_df_gene_expression['Category'] = "Plant_data" 
-
 counts_df_fungi = df_fungi.groupby(year_column).size().reset_index(name="counts")
-counts_df_fungi['Category'] = "Microbe_communities" 
-
 counts_df_bacteria = df_bacteria.groupby(year_column).size().reset_index(name="counts")
-counts_df_bacteria['Category'] = "Microbe_communities" 
 
 # Combine all data frames
 dfs = [
@@ -90,22 +67,11 @@ dfs = [
     counts_df_bacteria,
 ]
 df = pd.concat(dfs)
+print(df.columns)
+print(df.head())
 
 # Sum all data records year
-df = df.groupby(["Category", "Experimental_Year"])["counts"].sum().reset_index()
-
-# pivot data records 
-df_pivot = df.pivot_table(index="Experimental_Year", columns="Category", values="counts", fill_value=0)
-
-# colours from Figure 1 
-category_colors = {"Experimental_data": "#FFFF99", 
-                   "Field_data": "#D8BFD8",
-                   "Soil_data": "#FF9999",
-                   "Plant_data": "#ADD8E6",
-                   "Microbe_communities": "#C8E6C9"}
-
-category_order = ["Microbe_communities", "Plant_data", "Soil_data", "Field_data", "Experimental_data"]
-#category_order = ["Experimental_data", "Field_data", "Soil_data", "Plant_data", "Microbe_communities"]
+df = df.groupby("Experimental_Year")["counts"].sum().reset_index()
 
 # Create plot
 plt.rcParams.update({"text.usetex": True})
@@ -116,31 +82,18 @@ plt.yscale("log", base=10)
 plt.xticks(df["Experimental_Year"])
 plt.xlabel("Experimental year")
 plt.ylabel(r"\#Data records ($\log_{10}$)")
-
-# stacked barchart
-bars = []
-for category in category_order:
-    bars.append(
-        ax.bar(df_pivot.index, 
-               df_pivot[category], 
-               label=category, 
-               color=category_colors[category],
-               bottom=df_pivot[category_order[:category_order.index(category)]].sum(axis=1))
-    )
-
-for year in df_pivot.index:
-    total = df_pivot.loc[year].sum()
+bars = plt.bar(df["Experimental_Year"], df["counts"], color="#008080")
+for bar in bars:
+    yval = bar.get_height()
     plt.text(
-        year,
-        total,
-        format(int(total), ","),
+        bar.get_x() + bar.get_width() / 2,
+        yval,
+        format(int(yval), ","),
         va="bottom",
         ha="center",
         fontsize=10,
         color="black",
-        )
-
+    )
 plt.tight_layout()
-plt.savefig("stacked_data_records_per_year.jpg")
+plt.savefig("data_records_per_year.jpg")
 plt.show()
-print('done')
